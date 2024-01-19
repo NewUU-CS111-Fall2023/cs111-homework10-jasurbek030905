e3d4aa671d4aef1fa850215cde2a19eab06bc9b1
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2024-01-19 23:51:29 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -1,9 +1,24 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+int minRabbits(vector<int>& answers) {
+   unordered_map<int, int> countMap;
+    for (int answer : answers) {
+        countMap[answer]++;
+    }
+    int minRabbits = 0;
+    for (const auto& entry : countMap) {
+        int answer = entry.first;
+        int count = entry.second;
+        minRabbits += (count + answer - 1) / (answer + 1) * (answer + 1);
+    }
+    return minRabbits;
+}
+int task_7() {
+    vector<int> answers = {1, 1, 2};
+    int result = minRabbits(answers);
+    cout << "Minimum number of rabbits: " << result <<endl;
+    return 0;
+}
 
-class Problem7 {
-public:
-};
